4cd79d4322360adc43136fd1e445c87e3b012af3
Mark <Markgarvey42@gmail.com>
2021-08-16 23:26:50 -0500

    Hello World

---
+++ src/test/java/org/launchcode/ParkPals/ParkPalsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.ParkPals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkPalsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
