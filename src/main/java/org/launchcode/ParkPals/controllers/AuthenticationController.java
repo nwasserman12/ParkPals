289dad76b6322affdf725ed773089b0bd66a2d31
nwasserman12 <nickyann0823@aol.com>
2021-08-20 21:01:33 -0500

    have to link a local database to test if my stuff is working

--- src/main/java/org/launchcode/ParkPals/controllers/AuthenticationController.java
+++ src/main/java/org/launchcode/ParkPals/controllers/AuthenticationController.java
@@ -2,7 +2,10 @@ package org.launchcode.ParkPals.controllers;
 
 import org.launchcode.ParkPals.data.UserRepository;
 import org.launchcode.ParkPals.models.User;
+import org.launchcode.ParkPals.models.dto.LoginFormDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.HttpSession;
 import java.util.Optional;
@@ -31,5 +34,12 @@ public class AuthenticationController {
 
     private static void setUserSessionKey(HttpSession session, User user){
         session.setAttribute(userSessionKey, user.getId());
+    }
+
+    @GetMapping("/login")
+    public String displayLoginForm(Model model){
+        model.addAttribute(new LoginFormDTO());
+        model.addAttribute("title", "Log in");
+        return "login";
     }
 }
