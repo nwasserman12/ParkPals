475cd943f779e8ea045bfadaa23848cf4fb09031
Jelani Minter <jelaniminter90@gmail.com>
2021-08-21 16:39:42 -0500

    Added gradle dependencies and some models.

---
+++ src/main/java/org/launchcode/ParkPals/models/Park.java
@@ -0,0 +1,49 @@
+package org.launchcode.ParkPals.models;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+public class Park {
+
+    @NotBlank(message = "What is the name of the park?")
+    @Size(min = 2, max = 50)
+    private String parkName;
+
+    @NotBlank(message = "Where is the park located?")
+    @Size(min = 2, max = 50)
+    private String parkLocation;
+
+    @NotBlank(message = "Give a short description of the park.")
+    @Size(min = 2, max = 500)
+    private String parkDescription;
+
+    public Park(String parkName, String parkLocation, String parkDescription) {
+        this.parkName = parkName;
+        this.parkLocation = parkLocation;
+        this.parkDescription = parkDescription;
+    }
+
+    public String getParkName() {
+        return parkName;
+    }
+
+    public void setParkName(String parkName) {
+        this.parkName = parkName;
+    }
+
+    public String getParkLocation() {
+        return parkLocation;
+    }
+
+    public void setParkLocation(String parkLocation) {
+        this.parkLocation = parkLocation;
+    }
+
+    public String getParkDescription() {
+        return parkDescription;
+    }
+
+    public void setParkDescription(String parkDescription) {
+        this.parkDescription = parkDescription;
+    }
+}
