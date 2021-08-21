6b06111d4c803f0db03a3c7ef375740beae6dd57
nwasserman12 <nickyann0823@aol.com>
2021-08-20 20:40:07 -0500

    AuthenticationController added

---
+++ src/main/java/org/launchcode/ParkPals/controllers/AuthenticationController.java
@@ -0,0 +1,35 @@
+package org.launchcode.ParkPals.controllers;
+
+import org.launchcode.ParkPals.data.UserRepository;
+import org.launchcode.ParkPals.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.servlet.http.HttpSession;
+import java.util.Optional;
+
+public class AuthenticationController {
+
+    @Autowired
+    UserRepository userRepository;
+
+    private static final String userSessionKey = "user";
+
+    public User getUserFromSession(HttpSession session) {
+        Integer userId = (Integer) session.getAttribute(userSessionKey);
+        if (userId == null){
+            return null;
+        }
+
+        Optional<User> user = userRepository.findById(userId);
+
+        if (user.isEmpty()){
+            return null;
+        }
+
+        return user.get();
+    }
+
+    private static void setUserSessionKey(HttpSession session, User user){
+        session.setAttribute(userSessionKey, user.getId());
+    }
+}
