2cdca7da9479721bb61033f02d92bc2576288883
Mark Garvey <markgarvey42@gmail.com>
2021-08-23 11:05:54 -0500

    Minor conflict changes

--- src/main/java/org/launchcode/ParkPals/data/DogRepository.java
+++ src/main/java/org/launchcode/ParkPals/data/DogRepository.java
@@ -1,4 +1,8 @@
 package org.launchcode.ParkPals.data;
 
-public class DogRepository {
+import org.launchcode.ParkPals.models.Dog;
+import org.springframework.data.repository.CrudRepository;
+
+public interface DogRepository extends CrudRepository<Dog, Integer> {
 }
+
