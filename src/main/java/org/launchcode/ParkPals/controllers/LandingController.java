64f39445c4ec99733e2d2316a7f119a10f67c245
Mark Garvey <markgarvey42@gmail.com>
2021-08-24 17:22:36 -0500

    Bug fixing

--- src/main/java/org/launchcode/ParkPals/controllers/LandingController.java
+++ src/main/java/org/launchcode/ParkPals/controllers/LandingController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class LandingController {
 
-    @GetMapping("/hello")
+    @GetMapping
     public String landing() {
         return "landing";
     }
