737280a450dce030a6be74dd0413940f9444ee22
Mark <Markgarvey42@gmail.com>
2021-08-23 22:36:45 -0500

    Added logout mapping to authentication controller

--- src/main/java/org/launchcode/ParkPals/controllers/AuthenticationController.java
+++ src/main/java/org/launchcode/ParkPals/controllers/AuthenticationController.java
@@ -124,5 +124,11 @@ public class AuthenticationController {
 
         return "redirect:";
     }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request){
+        request.getSession().invalidate();
+        return "redirect:/login";
+    }
 }
 
