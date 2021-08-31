7df88489b1873957fa94e746854cea2341b16a31
Mark <Markgarvey42@gmail.com>
2021-08-30 21:22:15 -0500

    Added table that displays information about created events to home.html

--- src/main/java/org/launchcode/ParkPals/models/Event.java
+++ src/main/java/org/launchcode/ParkPals/models/Event.java
@@ -1,18 +1,24 @@
 package org.launchcode.ParkPals.models;
 
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+@Entity
 public class Event extends AbstractEntity {
 
     @NotNull
     private String name;
 
     @NotNull
+    @ManyToOne
     private User creator;
 
-    @NotNull
+    @ManyToOne
     private Park park;
 
     @NotNull
@@ -24,7 +30,8 @@ public class Event extends AbstractEntity {
     @NotNull
     private DogTemperament desiredTemperament;
 
-    private List<User> attendees;
+    @ElementCollection
+    private List<User> attendees = new ArrayList<>();
 
     public Event(String name, Park park, Date date, DogActivity desiredActivity, DogTemperament desiredTemperament) {
         this.name = name;
--- src/main/java/org/launchcode/ParkPals/models/User.java
+++ src/main/java/org/launchcode/ParkPals/models/User.java
@@ -33,8 +33,8 @@ public class User extends AbstractEntity {
     @ManyToMany
     private final List<Dog> dogs = new ArrayList<>();
 
-    @ManyToMany
-    private final List<Park> parks = new ArrayList<>();
+    @OneToMany
+    private final List<Event> userEvents = new ArrayList<>();
 
     @NotNull
     @Size(min = 2, message = "Username is required.")
@@ -106,16 +106,8 @@ public class User extends AbstractEntity {
         return dogs;
     }
 
-    public List<Park> getParks() {
-        return parks;
-    }
-
     public void addDog(Dog dog) {
         this.dogs.add(dog);
-    }
-
-    public void addPark(Park park) {
-        this.parks.add(park);
     }
 
     public boolean isMatchingPassword(String password) {
