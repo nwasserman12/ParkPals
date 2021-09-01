3ed3687162a6df115ccfa7364b600a93890cdd53
Mark Garvey <markgarvey42@gmail.com>
2021-09-01 16:16:30 -0500

    Minor changes

--- src/main/java/org/launchcode/ParkPals/ParkPalsApplication.java
+++ src/main/java/org/launchcode/ParkPals/ParkPalsApplication.java
@@ -1,7 +1,15 @@
 package org.launchcode.ParkPals;
 
+import org.launchcode.ParkPals.models.Park;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.logging.Logger;
 
 @SpringBootApplication
 public class ParkPalsApplication {
@@ -9,5 +17,4 @@ public class ParkPalsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ParkPalsApplication.class, args);
 	}
-
 }
---
+++ src/main/java/org/launchcode/ParkPals/controllers/ParkController.java
@@ -0,0 +1,9 @@
+package org.launchcode.ParkPals.controllers;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/parks")
+public class ParkController {
+
+}
