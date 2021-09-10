891e4b61cc5697cc1460c264a6c1c32dd4cc73ab
nwasserman12 <nickyann0823@aol.com>
2021-09-09 20:12:58 -0500

    html review

--- src/main/java/org/launchcode/ParkPals/controllers/ReviewController.java
+++ src/main/java/org/launchcode/ParkPals/controllers/ReviewController.java
@@ -1,4 +1,15 @@
 package org.launchcode.ParkPals.controllers;
 
+import org.launchcode.ParkPals.data.ReviewRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.persistence.Entity;
+
+@Entity
 public class ReviewController {
+
+    @Autowired
+    private ReviewRepository reviewRepository;
+
+
 }
