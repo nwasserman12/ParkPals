0a90b83a1c45c141b02e94e14a2514a3c60e7cff
nwasserman12 <nickyann0823@aol.com>
2021-09-09 20:35:40 -0500

    html review

--- src/main/java/org/launchcode/ParkPals/controllers/ReviewController.java
+++ src/main/java/org/launchcode/ParkPals/controllers/ReviewController.java
@@ -1,15 +1,30 @@
 package org.launchcode.ParkPals.controllers;
 
+import org.launchcode.ParkPals.data.ParkRepository;
 import org.launchcode.ParkPals.data.ReviewRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
-import javax.persistence.Entity;
-
-@Entity
+@Controller
 public class ReviewController {
 
     @Autowired
     private ReviewRepository reviewRepository;
+
+    @Autowired
+    private AuthenticationController authenticationController;
+
+    @Autowired
+    private ParkRepository parkRepository;
+
+    @GetMapping("/review")
+    public String ListOfReviews(Model model){
+        model.addAttribute("review", reviewRepository.findAll());
+        return "review";
+    }
+
 
 
 }
--- src/main/java/org/launchcode/ParkPals/models/Review.java
+++ src/main/java/org/launchcode/ParkPals/models/Review.java
@@ -4,6 +4,7 @@ import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.Date;
 import java.util.List;
 
@@ -23,11 +24,16 @@ public class Review extends AbstractEntity{
     @NotNull
     private ReviewStars numberOfStars;
 
-    public Review(User reviewer, Park park, Date reviewDate, ReviewStars numberOfStars) {
+    @NotNull
+    @Size(max=500, message = "Must not exceed 500 Characters")
+    private String description;
+
+    public Review(User reviewer, Park park, Date reviewDate, ReviewStars numberOfStars, String description) {
         this.reviewer = reviewer;
         this.park = park;
         this.reviewDate = reviewDate;
         this.numberOfStars = numberOfStars;
+        this.description = description;
     }
 
     public Review(){}
@@ -62,5 +68,13 @@ public class Review extends AbstractEntity{
 
     public void setNumberOfStars(ReviewStars numberOfStars) {
         this.numberOfStars = numberOfStars;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
