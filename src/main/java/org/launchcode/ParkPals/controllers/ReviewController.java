b0d75b28fa2e3ee25434b3af3cbf71c405b82a1d
nwasserman12 <nickyann0823@aol.com>
2021-09-16 21:18:33 -0500

    error on date.

--- src/main/java/org/launchcode/ParkPals/controllers/ReviewController.java
+++ src/main/java/org/launchcode/ParkPals/controllers/ReviewController.java
@@ -6,12 +6,15 @@ import org.launchcode.ParkPals.models.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("user")
@@ -33,13 +36,31 @@ public class ReviewController {
     }
 
     @GetMapping("{userId}/review/create-review")
-    public String displayCreateReviewForm(Model model, HttpServletRequest request){
+    public String displayCreateReviewForm(Model model, HttpServletRequest request, @ModelAttribute Park park){
         HttpSession session = request.getSession();
         User user = authenticationController.getUserFromSession(session);
         model.addAttribute(new Review());
         model.addAttribute("stars", ReviewStars.values());
         model.addAttribute("user", user);
+        model.addAttribute("park", park);
         return "review/create-review";
+    }
+
+    @PostMapping("{userId}/review/create-review")
+    public String processCreateReviewForm(@ModelAttribute @Valid Review newReview, @ModelAttribute Park park,
+                                          Errors errors, Model model, HttpServletRequest request){
+        HttpSession session = request.getSession();
+        User user = authenticationController.getUserFromSession(session);
+        if(errors.hasErrors()) {
+            model.addAttribute("stars", ReviewStars.values());
+            model.addAttribute("user", user);
+            model.addAttribute("park", park);
+            return "redirect:/review/create-review";
+        }
+        newReview.setPark(park);
+        newReview.setReviewer(user);
+        reviewRepository.save(newReview);
+        return "redirect:/review/review";
     }
 
 
