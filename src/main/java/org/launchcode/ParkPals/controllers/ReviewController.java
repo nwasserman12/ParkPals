fff41a53b461d7019762e325c38e4e9f52919ddb
nwasserman12 <nickyann0823@aol.com>
2021-09-21 20:06:32 -0500

    review now has proper park data from api

--- src/main/java/org/launchcode/ParkPals/controllers/ReviewController.java
+++ src/main/java/org/launchcode/ParkPals/controllers/ReviewController.java
@@ -7,10 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -35,13 +32,15 @@ public class ReviewController {
         return "review/review";
     }
 
-    @GetMapping("{userId}/review/create-review")
-    public String displayCreateReviewForm(Model model, HttpServletRequest request, @ModelAttribute Park park){
+    @GetMapping("{userId}/review/create-review/{placeId}")
+    public String displayCreateReviewForm(@PathVariable String placeId, Model model, HttpServletRequest request, Review review){
         HttpSession session = request.getSession();
         User user = authenticationController.getUserFromSession(session);
         model.addAttribute(new Review());
+        model.addAttribute(placeId);
         model.addAttribute("stars", ReviewStars.values());
         model.addAttribute("user", user);
+        Park park = parkRepository.findByPlaceId(placeId);
         model.addAttribute("park", park);
         return "review/create-review";
     }
